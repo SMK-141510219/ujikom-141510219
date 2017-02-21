2c097acad14adb8e3598b1729040c13030521b89
SMK-141510219 <141510219@smkassalaambandung.sch.id>
2017-02-21 17:16:35 +0700

    Hari Pertama

---
+++ database/migrations/2017_02_21_020259_create_kategori_lemburs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKategoriLembursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategori_lemburs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('kode_lembur')->unique();
+            $table->integer('jabatan_id')->unsigned();
+            $table->foreign('jabatan_id')->references('id')->on('jabatan')->OnDelete('cascade')->OnUpdate('cascade');
+            $table->integer('golongan_id')->unsigned();
+            $table->foreign('golongan_id')->references('id')->on('golongan')->OnDelete('cascade')->OnUpdate('cascade');
+            
+            $table->integer('besaran_uang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kategori_lemburs');
+    }
+}
